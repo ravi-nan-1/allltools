37195f3752b25597a236f8dc8e8196e39a4083f3
ravi-nan-1 <singhrajeev1470@gmail.com>
2026-09-04 20:32:15 +0530

    Fix build errors and update tools

--- src/app/tools/401k-calculator/401k-calculator.tsx
+++ src/app/tools/401k-calculator/401k-calculator.tsx
@@ -2,6 +2,6 @@
 
 import { AdvancedCalculator } from '@/components/tools/finance/advanced-calculators';
 
-export default function 401kCalculator( ) {
+export default function FourOhOneKCalculator() {
   return <AdvancedCalculator slug="401k-calculator" />;
 }
